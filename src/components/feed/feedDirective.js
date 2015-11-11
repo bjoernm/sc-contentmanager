19c9f80bc9531f14249d1a6cee1d91f41a3d5fcb
Albert Steckermeier <albert.steckermeier@gmail.com>
2015-11-11 10:54:47 +0100

    Fixed feed directive.

--- src/components/feed/feedDirective.js
+++ src/components/feed/feedDirective.js
@@ -2,21 +2,16 @@
     'use strict';
 
     angular
-        .module('scFeed', [])
+        .module('scFeed')
         .directive('scFeed', feedDirective);
 
-    //feedDirective.$inject = ['scFeedService', '$log'];
+    feedDirective.$inject = ['scFeedService', '$log'];
 
     function feedDirective(scFeedService, $log) {
         return {
             restrict: 'E',
-            replace: true,
             templateUrl: 'components/feed/feed.tpl.html',
             link: function (scope, element, attrs) {
-
-
-
-
             }
         };
 
