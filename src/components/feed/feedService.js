17f23b9e039effc54f7795b98ade0fe971438cee
Niklas Scholz <niklas.scholz@tum.de>
2015-12-11 12:35:54 +0100

    Breadcrumps, highlights von values und filter für Datum

--- src/components/feed/feedService.js
+++ src/components/feed/feedService.js
@@ -161,4 +161,24 @@
 
         return service;
     }
+
+    angular.module('scFeed').filter('toDate', function() {
+         return function (dateString) {
+             var date = '';
+             if(dateString != null){
+                 var dateObject = new Date(dateString);
+                 var day = dateObject.getDate();
+                 var month = dateObject.getMonth() + 1;
+                 if(day < 10){
+                    day = '0' + day;
+                 }
+                 if(month < 10){
+                    month = '0' + month;
+                 }
+                 date = day + '.' + month + '.' + dateObject.getFullYear();
+              }
+             return date;
+         };
+    });
+
 })();
